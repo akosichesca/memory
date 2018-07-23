5500e80f6a8a0f1f9916de729f4bae1d1b1cce8f
akosichesca <akosichesca@gmail.com>
2018-07-23 13:44:51 -0400

    chebyshev

--- hamming.sh
+++ hamming.sh
@@ -2,9 +2,9 @@
 
 #$ -q gpu           # Specify queue
 #$ -l gpu_card=2    # Specify parallel environment and legal core size
-#$ -N rare_hamming  # Specify job name
+#$ -N rare_hamming_20way_cheby  # Specify job name
 
 module load tensorflow/1.8
 module load python/3.6.0
 
-python train.py --memory_size=20000  --batch_size=16 --episode_width=5 --episode_length=50 --num_episodes=5000 --rep_dim=128
+python train.py --memory_size=2000  --batch_size=16 --episode_width=20 --episode_length=50 --num_episodes=5000 --rep_dim=128
